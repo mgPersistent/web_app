e67aa6e98206b2e9a2826bad700cd6b3950b80a2
mg <mangesh_ghodki@persistent.com>
2019-06-25 17:02:57 +0530

    first commit

---
+++ venv/Employee.py
@@ -0,0 +1,17 @@
+class Employee:
+
+    def __init__(self, id, name, contact, email, gender, pic):
+        self.id = id
+        self.name = name
+        self.contact = contact
+        self.email = email
+        self.gender = gender
+        self.pic = pic
+
+
+
+
+
+
+
+
